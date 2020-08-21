4f872e968e098ee34d55a6e088fc6a81d628d7e8
pranjaljain584 <62601082+pranjaljain584@users.noreply.github.com>
2020-08-21 16:39:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -110,14 +110,7 @@ function generateChart(wcArr){
             {
                 label: 'Zipfs Law',
                 data: zipsLaw ,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
+                backgroundColor: '#3282b8',
                 borderColor: 'blue',
                 borderWidth: 2
             }
